127e532ef593695029dc5bfcab577e5b61b1210f
Keshav Sharma <keshav1092.dev@gmail.com>
2026-08-18 20:02:31 +0530

    Lower Bound in sorted array

---
+++ Binary Search/Lower_bound.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lowerBound(vector<int>v,int target){
+    int low = 0 , high = v.size()-1 , ans = target;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(v[mid]==target) ans=mid;
+        else if(v[mid]>target) {
+            ans = mid;
+            high = mid-1;
+        }
+        else low=mid+1;
+    }
+    return ans;
+}
+int main(){
+    vector<int> v = {1,2,5,7,10,22,40};
+    int t = 8;
+    cout << lowerBound(v,t);
+    return 0; 
+}
