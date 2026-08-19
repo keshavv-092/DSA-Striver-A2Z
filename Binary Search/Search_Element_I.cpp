4907b76351c5798c605b6e294f6a1a1878d4a66d
Keshav Sharma <keshav1092.dev@gmail.com>
2026-08-19 21:51:24 +0530

    Search Element in rotated sorted arrat  - I

---
+++ Binary Search/Search_Element_I.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int search(vector<int>v,int target){
+    int low = 0;
+    int high = v.size()-1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(v[mid]==target){
+            return mid ; 
+        }        
+        if(v[low]<=v[mid]){
+            if(v[low]<=target && target <= v[mid]){
+                high = mid -1;
+            }
+            else low = mid +1;
+        }
+        else{
+            if(v[mid]<=target && v[high]>=target){
+                low = mid +1;
+            }
+            else high = mid -1;
+        }
+    }
+    return -1;
+}
+int main(){
+    vector<int> v = {7,8,9,10,4,5,6};
+    int t =9;
+    cout << search(v,t) ;
+    return 0;
+}
