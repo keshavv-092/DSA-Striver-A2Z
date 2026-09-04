471d85ec8d97a0de5fc731b59697e5deb2806d5e
Keshav Sharma <keshav1092.dev@gmail.com>
2026-09-04 22:09:17 +0530

    Longest Common Prefix

---
+++ Strings/Longest_common_prefix.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+string longestCommonPrefix(vector<string> &strs)
+{
+    sort(strs.begin(), strs.end());
+    string first = strs[0];
+    string last = strs[strs.size() - 1];
+    string ans;
+    int minlength = min(first.size(), last.size());
+    for (int i = 0; i < minlength; i++)
+    {
+        if (first[i] != last[i])
+            break;
+        ans += first[i];
+    }
+    return ans;
+}
+int main(){
+    vector<string> s = {"flower","flight","flow"};
+    cout << longestCommonPrefix(s) << endl;
+    return 0;
+}
