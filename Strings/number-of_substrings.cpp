bfccdd7e7338e2e8c74ccd667318da4a3f558df2
Keshav Sharma <keshav1092.dev@gmail.com>
2026-09-14 20:32:58 +0530

    Count number of substrings

---
+++ Strings/number-of_substrings.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int atMostKDistinct(string s, int k) {
+    int left = 0, res = 0;
+    unordered_map<char, int> freq;
+    for (int right = 0; right < s.size(); right++) {
+        freq[s[right]]++;
+        while (freq.size() > k) {
+            freq[s[left]]--;
+            if (freq[s[left]] == 0) freq.erase(s[left]);
+            left++;
+        }
+        res += (right - left + 1);
+    }
+    return res;
+}
+int countSubstrings(string s, int k) {
+    return atMostKDistinct(s, k) - atMostKDistinct(s, k - 1);
+}
+
+int main() {
+    string s = "pqpqs";
+    int k = 2;
+    cout << "Count: " << countSubstrings(s, k) << endl; 
+    return 0;
+}
