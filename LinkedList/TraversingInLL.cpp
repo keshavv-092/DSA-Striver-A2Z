222f1554b9898b7f239b7563260bc195436afb88
Keshav Sharma <keshav1092.dev@gmail.com>
2026-09-19 18:21:44 +0530

    Traversal in Linked List

---
+++ LinkedList/TraversingInLL.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data1 , Node* next1){
+        data = data1;
+        next = next1;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+Node* ConvertArr2LL(vector<int>v){
+    Node* head = new Node(v[0]);
+    Node* mover = head;
+    for(int i=1; i<v.size(); i++){
+        Node* temp = new Node(v[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+int main(){
+    vector<int> v = {12,4,6,8,10};
+    Node* head = ConvertArr2LL(v);
+    Node* temp = head;
+    while(temp){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    return 0;
+}
