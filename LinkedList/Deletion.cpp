10321b39d36dfb450f34eabcac51120e439fc147
Keshav Sharma <keshav1092.dev@gmail.com>
2026-09-23 13:06:34 +0530

    Deletion Operations on Linked List

---
+++ LinkedList/Deletion.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int info;
+    Node *next;
+    Node(int data1, Node *next1)
+    {
+        info = data1;
+        next = next1;
+    }
+    Node(int data1)
+    {
+        info = data1;
+        next = nullptr;
+    }
+};
+Node *DeleteHead(Node *head)
+{
+    if (head == NULL)
+    {
+        cout << "Empty Linked List";
+        return head;
+    }
+    Node *temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+Node *DeleteTail(Node *head){
+    if(head == NULL){
+        cout<<"empty Linked List";
+        return head;
+    }
+    Node *temp=head;
+    while(temp->next->next){
+        temp = temp->next;
+    }
+    temp->next = NULL;
+    return head;
+}
+Node *DeleteSpecific(Node *head , int pos){
+    if(head == NULL){
+        cout << "Empty Linked List ";
+        return head;
+    }
+    Node *temp = head;
+    for(int i=1; i<pos-1; i++){
+        temp = temp->next;
+    }
+    temp->next = temp->next->next;
+    return head;
+}
+Node* convertToLL(vector<int>v){
+    Node *head = new Node(v[0]);
+    Node *mover = head;
+    for(int i=1; i<v.size(); i++){
+        Node *temp = new Node(v[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+void traverse(Node *head){
+    Node *temp = head;
+    while(temp){
+        cout<<temp->info<<" ";
+        temp = temp->next;
+    }
+    cout <<endl;
+}
+int main(){
+    vector<int> v = {2,3,4,5,10,20};
+    Node *head = convertToLL(v);
+    head = DeleteSpecific(head,2);
+    traverse(head);
+}
